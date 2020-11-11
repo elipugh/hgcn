3b424aca96610634a2f01f570b90f9089f7acf12
Eli <epugh@stanford.edu>
2020-11-11 01:27:12 -0500

    added more history

--- experiment.py
+++ experiment.py
@@ -142,7 +142,8 @@ def run_experiment(model, manifold, dim, dataset="cora", log_freq=5, cuda=-1,
                "eval_freq":args.eval_freq,
                "model":args.model,
                "dataset":args.dataset,
-               "dim":args.dim}
+               "dim":args.dim,
+               "manifold":args.manifold}
     for epoch in range(args.epochs):
         t = time.time()
         model.train()
