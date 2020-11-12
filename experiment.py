3b87063141203cfaf7c82ab7b760617a97a9f47a
Eli <epugh@stanford.edu>
2020-11-12 18:16:49 -0500

    small logging edit

--- experiment.py
+++ experiment.py
@@ -251,7 +251,6 @@ def run_experiment(model, manifold, dim, dataset="cora", log_freq=5, cuda=-1,
         torch.save(model.state_dict(), os.path.join(save_dir, 'model.pth'))
         logging.info(f"Saved model in {save_dir}")
     logging.shutdown()
-    del(logging)
     print("\n")
     return history
 
