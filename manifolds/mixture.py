49838513e2f4620b09d4bd7532a895dfac710ebb
Eli <epugh@stanford.edu>
2020-11-12 01:45:20 -0500

    change to work with 0 fracs

--- manifolds/mixture.py
+++ manifolds/mixture.py
@@ -28,68 +28,146 @@ class Mixture(Manifold):
 
     def sqdist(self, x, y, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.sqdist(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
-        euc = self.Euclidean.sqdist(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.sqdist(x[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
-        total = (hyper**2 + euc**2 + poin**2)**.5
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.sqdist(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.sqdist(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.sqdist(x[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
         ##sum lol
-        return hyper.view(-1) + euc + poin
+        sum = 0
+        if self.Fractions[0] != 0: sum += hyper.view(-1)
+        if self.Fractions[1] != 0: sum += euc
+        if self.Fractions[2] != 0: sum += poin
+        return sum
 
     def proj(self, x, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.proj(x[..., :self.Split[0]], c)
-        euc = self.Euclidean.proj(x[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.proj(x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.proj(x[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.proj(x[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.proj(x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def proj_tan(self, u, x, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.proj_tan(u[..., :self.Split[0]], x[..., :self.Split[0]], c)
-        euc = self.Euclidean.proj_tan(u[..., self.Split[0] : self.Split[1]], x[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.proj_tan(u[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.proj_tan(u[..., :self.Split[0]], x[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.proj_tan(u[..., self.Split[0] : self.Split[1]], x[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.proj_tan(u[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def proj_tan0(self, u, c):
         self.rescale_dims(u)
-        hyper = self.Hyperboloid.proj_tan0(u[..., :self.Split[0]], c)
-        euc = self.Euclidean.proj_tan0(u[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.proj_tan0(u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.proj_tan0(u[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.proj_tan0(u[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.proj_tan0(u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def expmap(self, u, x, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.expmap(u[..., :self.Split[0]], x[..., :self.Split[0]], c)
-        euc = self.Euclidean.expmap(u[..., self.Split[0] : self.Split[1]], x[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.expmap(u[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.expmap(u[..., :self.Split[0]], x[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.expmap(u[..., self.Split[0] : self.Split[1]], x[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.expmap(u[..., self.Split[1] : self.Split[2]], x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
 
     def logmap(self, x, y, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.logmap(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
-        euc = self.Euclidean.logmap(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.logmap(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.logmap(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.logmap(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.logmap(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def expmap0(self, u, c):
         self.rescale_dims(u)
-        hyper = self.Hyperboloid.expmap0(u[..., :self.Split[0]], c)
-        euc = self.Euclidean.expmap0(u[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.expmap0(u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.expmap0(u[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.expmap0(u[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.expmap0(u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def logmap0(self, x, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.logmap0(x[..., :self.Split[0]], c)
-        euc = self.Euclidean.logmap0(x[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.logmap0(x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.logmap0(x[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.logmap0(x[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.logmap0(x[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def mobius_add(self, x, y, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.mobius_add(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
-        euc = self.Euclidean.mobius_add(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.mobius_add(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.mobius_add(x[..., :self.Split[0]], y[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.mobius_add(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.mobius_add(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def mobius_matvec(self, m, x, c):
@@ -97,10 +175,12 @@ class Mixture(Manifold):
         'Seperate out Manifolds, project:'
 
         'hyperboloid'
-        hyper_u = self.Hyperboloid.logmap0(x[..., :self.Split[0]], c)
-
-        'accumulate'
-        accumulated_u = torch.cat([hyper_u, x[..., self.Split[0] : self.Split[2]]], dim=1)
+        if self.Fractions[0] != 0:
+            hyper_u = self.Hyperboloid.logmap0(x[..., :self.Split[0]], c)
+            'accumulate'
+            accumulated_u = torch.cat([hyper_u, x[..., self.Split[0] : self.Split[2]]], dim=1)
+        else:
+            accumulated_u = x
 
         'multiply'
         mu = accumulated_u @ m.transpose(-1, -2)
@@ -110,21 +190,27 @@ class Mixture(Manifold):
         'now unscrew the vector'
 
         'hyperboloid'
-        # print(mu[..., :self.Split[0]].shape)
-        hyper_final = self.Hyperboloid.expmap0(mu[..., :self.Split[0]], c)
+        if self.Fractions[0] != 0:
+            hyper_final = self.Hyperboloid.expmap0(mu[..., :self.Split[0]], c)
 
+        if self.Fractions[2] != 0:
+            'poincare'
+            sqrt_c = c ** 0.5
+            poinc_x_norm = x[..., self.Split[1] : self.Split[2]].norm(dim=-1, keepdim=True, p=2).clamp_min(self.min_norm)
+            poinc_mx_norm = mu[..., self.Split[1] : self.Split[2]].norm(dim=-1, keepdim=True, p=2).clamp_min(self.min_norm)
+            res_c = tanh(poinc_x_norm / poinc_x_norm * artanh(sqrt_c * poinc_x_norm)) * mu[..., self.Split[1] : self.Split[2]] / (poinc_x_norm * sqrt_c)
+            cond = (mu[..., self.Split[1] : self.Split[2]] == 0).prod(-1, keepdim=True, dtype=torch.uint8)
+            res_0 = torch.zeros(1, dtype=res_c.dtype, device=res_c.device)
+            poincare_final = torch.where(cond, res_0, res_c)
 
-        'poincare'
-        sqrt_c = c ** 0.5
-        poinc_x_norm = x[..., self.Split[1] : self.Split[2]].norm(dim=-1, keepdim=True, p=2).clamp_min(self.min_norm)
-
-        poinc_mx_norm = mu[..., self.Split[1] : self.Split[2]].norm(dim=-1, keepdim=True, p=2).clamp_min(self.min_norm)
-        res_c = tanh(poinc_x_norm / poinc_x_norm * artanh(sqrt_c * poinc_x_norm)) * mu[..., self.Split[1] : self.Split[2]] / (poinc_x_norm * sqrt_c)
-        cond = (mu[..., self.Split[1] : self.Split[2]] == 0).prod(-1, keepdim=True, dtype=torch.uint8)
-        res_0 = torch.zeros(1, dtype=res_c.dtype, device=res_c.device)
-        poincare_final = torch.where(cond, res_0, res_c)
-
-        new_mu = torch.cat([hyper_final, mu[...,self.Split[0] : self.Split[1]], poincare_final], dim=1)
+        if self.Fractions[0] == 0:
+            new_mu = torch.cat([mu[...,self.Split[0] : self.Split[1]], poincare_final], dim=1)
+        if self.Fractions[1] == 0:
+            new_mu = torch.cat([hyper_final, poincare_final], dim=1)
+        if self.Fractions[2] == 0:
+            new_mu = torch.cat([hyper_final, mu[...,self.Split[0] : self.Split[1]]], dim=1)
+        else:
+            new_mu = torch.cat([hyper_final, mu[...,self.Split[0] : self.Split[1]], poincare_final], dim=1)
 
         return new_mu
 
@@ -146,29 +232,62 @@ class Mixture(Manifold):
         length = len(x[0,:])
         if length != self.length:
             self.Split[0] = int(self.Fractions[0] * length)
-            self.Split[1] = int(self.Fractions[1] * length) + self.Split[0]
+            if self.Fractions[2] == 0:
+                self.Split[1] = length
+            else:
+                self.Split[1] = int(self.Fractions[1] * length) + self.Split[0]
             self.Split[2] = length
             self.length = length
 
 
     def ptransp(self, x, y, u, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.ptransp(x[..., :self.Split[0]], y[..., :self.Split[0]], u[..., :self.Split[0]], c)
-        euc = self.Euclidean.ptransp(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], u[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.ptransp(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.ptransp(x[..., :self.Split[0]], y[..., :self.Split[0]], u[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.ptransp(x[..., self.Split[0] : self.Split[1]], y[..., self.Split[0] : self.Split[1]], u[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.ptransp(x[..., self.Split[1] : self.Split[2]], y[..., self.Split[1] : self.Split[2]], u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
     def ptransp0(self, x, u, c):
         self.rescale_dims(x)
-        hyper = self.Hyperboloid.ptransp0(x[..., :self.Split[0]], u[..., :self.Split[0]], c)
-        euc = self.Euclidean.ptransp0(x[..., self.Split[0] : self.Split[1]], u[..., self.Split[0] : self.Split[1]], c)
-        poin = self.Poincare.ptransp0(x[..., self.Split[1] : self.Split[2]], u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] != 0:
+            hyper = self.Hyperboloid.ptransp0(x[..., :self.Split[0]], u[..., :self.Split[0]], c)
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.ptransp0(x[..., self.Split[0] : self.Split[1]], u[..., self.Split[0] : self.Split[1]], c)
+        if self.Fractions[2] != 0:
+            poin = self.Poincare.ptransp0(x[..., self.Split[1] : self.Split[2]], u[..., self.Split[1] : self.Split[2]], c)
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
         return torch.cat([hyper, euc, poin], dim = 1)
 
 
     def normalize(self, p):
-        self.rescale_dims(p)
-        return torch.cat([p[...,:self.Split[0]], self.Euclidean.normalize(p[...,self.Split[0]:self.Split[1]]), p[...,self.Split[1]:self.Split[2]]], dim=1)
+        self.rescale_dims(x)
+        if self.Fractions[0] != 0:
+            hyper = p[...,:self.Split[0]]
+        if self.Fractions[1] != 0:
+            euc = self.Euclidean.normalize(p[...,self.Split[0]:self.Split[1]])
+        if self.Fractions[2] != 0:
+            poin = p[...,self.Split[1]:self.Split[2]]
+        if self.Fractions[0] == 0:
+            return torch.cat([euc, poin], dim = 1)
+        if self.Fractions[1] == 0:
+            return torch.cat([hyper, poin], dim = 1)
+        if self.Fractions[2] == 0:
+            return torch.cat([hyper, euc], dim = 1)
+        return torch.cat([hyper, euc, poin], dim = 1)
 
     # def to_poincare(self, x, c):
     #     K = 1. / c
